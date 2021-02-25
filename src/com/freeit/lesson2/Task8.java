1ad28e61b63950891fd42ecec9f9746748340eea
horsm <horsm@mail.ru>
2021-02-25 11:01:16 +0300

    solving Task8

---
+++ src/com/freeit/lesson2/Task8.java
@@ -0,0 +1,19 @@
+package com.freeit.lesson2;
+
+/*
+8)Начав тренировки, спортсмен в первый день пробежал 10 км.
+Каждый день он увеличивал дневную норму на 10% нормы предыдущего дня.
+Какой суммарный путь пробежит спортсмен за 7 дней?
+ */
+public class Task8 {
+    public static void main(String[] args) {
+        double norma = 10;
+        double sum = 10;
+        for (int i = 1; i < 7; i++) {
+            norma = norma * 1.1;
+            sum += norma;
+        }
+
+        System.out.format("Суммарный путь = %.3f", sum);
+    }
+}
