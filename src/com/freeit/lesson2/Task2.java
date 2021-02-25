73106a5cfe2e567fdb07d54ff0d1c17a11af4052
horsm <horsm@mail.ru>
2021-02-25 11:23:17 +0300

    solving Task2

---
+++ src/com/freeit/lesson2/Task2.java
@@ -0,0 +1,28 @@
+package com.freeit.lesson2;
+
+import java.util.Scanner;
+
+/*
+2) Треугольник существует только тогда, когда сумма любых двух его сторон больше третьей.
+Определить существует ли такой треугольник.
+Дано: a, b, c –стороны предполагаемого треугольника.
+Требуется сравнить длину каждого отрезка-стороны с суммой двух других.
+Если хотя бы в одном случае отрезок окажется больше суммы двух других, то треугольника с такими сторонами не существует.
+ */
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите сторону A: ");
+        int a = sc.nextInt();
+        System.out.print("Введите сторону B: ");
+        int b = sc.nextInt();
+        System.out.print("Введите сторону C: ");
+        int c = sc.nextInt();
+
+        if ((a + b > c) && (a + c > b) && (b + c > a)) {
+            System.out.println("Такой треугольник существует!");
+        } else {
+            System.out.println("Такой треугольник не существует!");
+        }
+    }
+}
