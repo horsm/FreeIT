208e562004b38f2a41d53765cc7c7ef40389a806
horsm <horsm@mail.ru>
2021-02-25 08:52:26 +0300

    solving Task6

---
+++ src/com/freeit/lesson2/Task6.java
@@ -0,0 +1,25 @@
+package com.freeit.lesson2;
+
+import java.util.Scanner;
+
+/*
+6) Даны 2 числа. Вывести большее из них
+ */
+public class Task6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        int numberOne = sc.nextInt();
+        System.out.print("Введите второе число: ");
+        int numberTwo = sc.nextInt();
+
+        if (numberOne > numberTwo) {
+            System.out.println("Большее число: " + numberOne);
+        } else if (numberOne < numberTwo) {
+                System.out.println("Большее число: " + numberTwo);
+        } else {
+            System.out.println("Числа одинаковые!");
+        }
+
+    }
+}
