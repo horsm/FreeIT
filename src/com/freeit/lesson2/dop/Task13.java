bbfa584ae1f27165750c403114b763032e9abf40
horsm <horsm@mail.ru>
2021-03-02 20:22:59 +0300

    add Task13

---
+++ src/com/freeit/lesson2/dop/Task13.java
@@ -0,0 +1,16 @@
+package com.freeit.lesson2.dop;
+
+/*
+13)Напишите программу вывода всех четных чисел от 2 до 100 включительно
+ */
+
+public class Task13 {
+    public static void main(String[] args) {
+        System.out.println("Все четные числа от 2 до 100 включительно:");
+        for (int i = 2; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
