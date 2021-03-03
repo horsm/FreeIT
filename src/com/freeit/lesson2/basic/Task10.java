e34f0cc2e628048ca16d6abca0f103be38e3fe43
horsm <horsm@mail.ru>
2021-03-03 08:53:18 +0300

    Факториал числа в границах от 10 до 15

---
+++ src/com/freeit/lesson2/basic/Task10.java
@@ -0,0 +1,28 @@
+package com.freeit.lesson2.basic;
+
+/*
+Задание 10
+Посчитать факториал числа в границах от 10 до 15 (не используя рекурсию).
+ */
+
+public class Task10 {
+    public static void main(String[] args) {
+        long factorial = 1;
+
+        for (int i = 10; i <= 15; i++) {
+            int n = i;
+            int x = i;
+
+            while(x != 0) {
+                if (x == 0) {
+                    factorial = 1;
+                } else {
+                    factorial = factorial * x;
+                    x--;
+                }
+            }
+            System.out.println("Факториал числа " + n + " = " + factorial);
+            factorial = 1;
+        }
+    }
+}
