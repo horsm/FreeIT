18e527599f3c3f5a9e39a021225fd670d7eb4329
horsm <horsm@mail.ru>
2021-04-06 10:33:22 +0300

    Создать список оценок учеников

---
+++ src/com/freeit/lesson7/Task29ListRratings.java
@@ -0,0 +1,30 @@
+package com.freeit.lesson7;
+
+/*
+Задание 29
+Создать список оценок учеников с помощью ArrayList, заполнить случайными оценками.
+Удалить неудовлетворительные оценки из списка.
+ */
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Task29ListRratings {
+    public static void main(String[] args) {
+        ArrayList <Integer> listOfRatings = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            listOfRatings.add(new Random().nextInt(11));
+        }
+        System.out.println("Исходный список оценок учеников:");
+        System.out.println(listOfRatings.toString());
+
+        for (int i = 0; i < listOfRatings.size(); i++) {
+            if (listOfRatings.get(i) < 3) {
+                listOfRatings.remove(i);
+            }
+        }
+        System.out.println("Исправленный список оценок учеников:");
+        System.out.println(listOfRatings.toString());
+
+    }
+}
